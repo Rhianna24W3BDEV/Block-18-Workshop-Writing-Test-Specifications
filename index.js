0a9be60f38b3f461d20024d134a03ede1a5befef
Rhianna Heflin <rhiannaheflin43@gmail.com>
2024-03-13 12:31:20 -0400

    final work

---
+++ index.js
@@ -0,0 +1,31 @@
+// Unit Tests
+
+// A functiom called "multiplication" that returns the product of the two input numbers.
+
+/*
+Expect multiplication (5,10) to be a number
+Expect multiplication (5,10) to equal 50
+Expect multiplication (c,a) to be an error
+*/
+
+//A function called "concatOdds" takes two arrays of integers as arguments. 
+//It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+/*
+Expect concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to result in [-1, 1, 3, 9, 15]
+Expect concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to show mulitples of the same number 
+Expect concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to show numbers that arent odds like 2 and 4
+Expect concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to only show one of the same number
+Expect concatOdds ([a, 2, d], [e, d, d, d, 4, f, g]) to be an error 
+*/
+
+// Functional Test 
+
+/*
+Prompt the user to login or proceed as guest before they begin adding items to their cart
+If the cart is empty and the user clicks checkout display a message saying "nothing in cart"
+The cart should display all items user has selected before procceding to payment
+When user clicks "remove item" button the item will be removed from the cart
+When the user enters an invaild password it will display a error message
+*/
+
